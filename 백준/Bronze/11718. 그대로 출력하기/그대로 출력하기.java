e412e5662337f36bf98ba6b74d62420f567606f5
jihyang Lee <94888511+jihyang1106@users.noreply.github.com>
2023-05-21 22:24:54 +0900

    Update 그대로 출력하기.java

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//https://www.acmicpc.net/problem/11718
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
