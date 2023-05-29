c41d01dfe32dfe5ba2a9bff1f60f4a675dd4de75
jihyang Lee <94888511+jihyang1106@users.noreply.github.com>
2023-05-29 16:55:32 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Main {
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(br.readLine()));
+    }
+    
+    public static int solution(String input) {
+        int length = input.toCharArray().length;
+        return length;
+    }
+}
