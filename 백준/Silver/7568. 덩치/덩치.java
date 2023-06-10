2008d43809e4ed2ff7273f1fbc7198641214e53f
jihyang Lee <94888511+jihyang1106@users.noreply.github.com>
2023-06-10 17:12:10 +0900

    Update 덩치.java
    스트림으로 변환

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -2,11 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] size = new int [N][2];
@@ -21,16 +22,15 @@ public class Main {
     }
 
     public static String solution(int[][] size, int N) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<N;i++) {
+       return IntStream.range(0, N)
+        .mapToObj(i -> {
             int rank = 1;
-            for(int j=0;j<N;j++) {
-                if(i == j) continue;  // 같은 사람 비교 xx
-                // i 번째 사람과 j번째 사람을 비교해 i가 j보다 덩치가 작을 때 rank ++
-                if(size[i][0] < size[j][0] && size[i][1] < size[j][1]) rank++;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (size[i][0] < size[j][0] && size[i][1] < size[j][1]) rank++;
             }
-            sb.append(rank).append(" ");
-        }
-        return sb.toString();
+            return String.valueOf(rank);
+        })
+        .collect(Collectors.joining(" "));
     }
 }
