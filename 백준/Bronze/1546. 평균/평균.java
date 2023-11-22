cf767d79bbea4291683da4a4e843976cbc2b9cf5
jihyang Lee <94888511+jihyang1106@users.noreply.github.com>
2023-11-23 06:34:08 +0900

    Update 평균.java

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,27 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] score = new int[N];
-        for(int i=0;i<N;i++) {
-            score[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(solution(N, score));
-    }
+        int sum = 0;
+        int max = 0;
 
-    public static Double solution(int N, int[] score) {
-        Double answer = 0.0;
-        int max = 0; // 최댓값
-        int sum = 0; // 점수 평균
-        for(int i=0;i<score.length;i++) {
-            if (max < score[i]) max = score[i];
-            sum += score[i];
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            int subjectValue = Integer.parseInt(token);
+            max = Math.max(max, subjectValue);
+            sum += subjectValue;
         }
-        answer = (double) sum;
-        return answer*100/max/N;
+        System.out.println(sum*100.0/max/N);
     }
 }
